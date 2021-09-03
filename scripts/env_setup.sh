0dd774f801dab609490c4fd745a797efe44d8840
kweaver87 <28742312+KWeaver87@users.noreply.github.com>
2021-09-03 09:54:11 -0400

    Updates setup script

--- scripts/env_setup.sh
+++ scripts/env_setup.sh
@@ -12,7 +12,7 @@ WHICH_ZSH=$(which zsh)
 # Update and install apt packages
 sudo apt update
 sudo apt upgrade -y
-sudo apt install build-essential git zsh curl wget aptitude mc -y
+sudo apt install build-essential git zsh curl wget aptitude mc fail2ban -y
 
 # Set zsh to default shell
 sudo chsh -s $WHICH_ZSH $USER
@@ -27,9 +27,6 @@ source ~/.zshrc
 # Download and set desired zsh theme
 curl -fsSL http://raw.github.com/zakaziko99/agnosterzak-ohmyzsh-theme/master/agnosterzak.zsh-theme > $ZSH_CUSTOM/themes/agnosterzak.zsh-theme
 sed -i 's:="robbyrussell:="agnosterzak:' ~/.zshrc
-echo "
-alias l="exa -lah"
-" | tee -a ~/.zshrc > /dev/null
 
 # Install Nix single-user and update $PATH
 curl -L https://nixos.org/nix/install | sh -s -- --no-daemon
@@ -38,16 +35,20 @@ source ~/.zshrc
 # Install nix packages
 nix-env -iA nixpkgs.bat nixpkgs.bottom nixpkgs.bpytop nixpkgs.du-dust nixpkgs.cht-sh nixpkgs.exa nixpkgs.fd nixpkgs.ripgrep nixpkgs.sd nixpkgs.bandwhich
 
+# Alias for l command
+echo "
+alias l=\"exa -lah\"
+" | tee -a ~/.zshrc > /dev/null
+
 # Set up oh-my-zsh and nix binaries for root
-# TODO: UNTESTED
 sudo cp -r $USERHOME/.oh-my-zsh /root/
 sudo cp $USERHOME/.zshrc /root/
-sudo sed -i 's:/home/ubuntu:/root:g' /root/.zshrc
+sudo sed -i "s:$USERHOME:/root:g" /root/.zshrc
 sudo mkdir /root/.nix-profile
 sudo ln -s $USERHOME/.nix-profile/bin /root/.nix-profile/bin
 echo "
-if [ -d "/root/.nix-profile/bin" ] ; then
-  PATH="/root/.nix-profile/bin:\$PATH"
+if [ -d \"/root/.nix-profile/bin\" ] ; then
+  PATH=\"/root/.nix-profile/bin:\$PATH\"
 fi" | sudo tee -a /root/.zshrc > /dev/null
 
 # Reboot machine immediately
