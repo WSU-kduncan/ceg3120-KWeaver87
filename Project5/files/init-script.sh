0627dbe3dedc11c111dbe40a0a583c0974e1e03c
KWeaver87 <28742312+KWeaver87@users.noreply.github.com>
2021-11-10 12:51:18 -0500

    Removes extra &&

--- Project5/files/init-script.sh
+++ Project5/files/init-script.sh
@@ -1,20 +1,20 @@
 #!/bin/bash -xe
 # Upgrade installed packages first
-apt update &&
-apt full-upgrade -y &&
+apt update
+apt full-upgrade -y
 
 # Install desired packages
-apt install -y git fish doc-base curl ripgrep fd-find fail2ban w3m &&
+apt install -y git fish doc-base curl ripgrep fd-find fail2ban w3m
 
 # Append friendly names to hosts
 echo "
 10.0.0.8 proj5-proxy
 10.0.0.11 httpd-1
-10.0.0.12 httpd-2" >> /etc/hosts &&
+10.0.0.12 httpd-2" >> /etc/hosts
 
 # Set up Starship prompt on fish shell
-sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y &&
-echo "starship init fish | source" >> /etc/fish/config.fish &&
+sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y
+echo "starship init fish | source" >> /etc/fish/config.fish
 
 # Make fish default shell
 sed -i 's#/bin/bash#/usr/bin/fish#g' /etc/passwd
