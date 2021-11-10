37877b0039a33618bc15e3cf93180ffcddff40e2
KWeaver87 <28742312+KWeaver87@users.noreply.github.com>
2021-11-10 11:32:48 -0500

    Fixes EOF error

--- Project5/files/init-script.sh
+++ Project5/files/init-script.sh
@@ -7,9 +7,9 @@ apt-get upgrade -y &&
 apt-get install -y git fish doc-base curl ripgrep fd-find fail2ban w3m &&
 
 # Append friendly names to hosts
-echo "
-10.0.0.8 proj5-proxy
-10.0.0.11 httpd-1
+echo " \
+10.0.0.8 proj5-proxy \
+10.0.0.11 httpd-1 \
 10.0.0.12 httpd-2" >> /etc/hosts &&
 
 # Set up Starship prompt on fish shell
