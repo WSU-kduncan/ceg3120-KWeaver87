ccc44ddb83158f23a629073f5f0863b129ef7ba3
KWeaver87 <28742312+KWeaver87@users.noreply.github.com>
2021-11-10 11:28:01 -0500

    Fixes missing && in init script

--- Project5/files/init-script.sh
+++ Project5/files/init-script.sh
@@ -4,13 +4,13 @@ apt-get update &&
 apt-get upgrade -y &&
 
 # Install desired packages
-apt-get install -y git fish doc-base curl ripgrep fd-find fail2ban &&
+apt-get install -y git fish doc-base curl ripgrep fd-find fail2ban w3m &&
 
 # Append friendly names to hosts
 echo "
 10.0.0.8 proj5-proxy
 10.0.0.11 httpd-1
-10.0.0.12 httpd-2" >> /etc/hosts
+10.0.0.12 httpd-2" >> /etc/hosts &&
 
 # Set up Starship prompt on fish shell
 sh -c "$(curl -fsSL https://starship.rs/install.sh)" -- -y &&
